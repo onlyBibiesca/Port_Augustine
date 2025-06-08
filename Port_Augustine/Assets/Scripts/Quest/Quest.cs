e6403a68fde90fdee302dba4658fc4b999896645
onlyBibiesca <vikktourr@gmail.com>
2025-06-08 14:09:21 +0800

    Quest System Part 1

---
+++ Port_Augustine/Assets/Scripts/Quest/Quest.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quests/Quest")]
+public class Quest : ScriptableObject
+{
+    public string questID;
+    public string questName;
+    public string description;
+    public List<QuestObjective> objectives;
+
+    private void OnValidate()
+    {
+        if(string.IsNullOrEmpty(questID))
+            questID = questName + Guid.NewGuid().ToString();
+    }
+
+    
+}
+
+[System.Serializable]
+public class QuestObjective
+{
+    public string objectiveID; //match id
+    public string description;
+    public ObjectiveType type;
+    //public string objectiveDescription;
+    public int requiredAmount;
+    public int currentAmount;
+
+    public bool isCompleted => currentAmount >= requiredAmount;
+
+}
+
+public enum ObjectiveType { CollectItem, ReachLocation, TalkNPC, Custom }
+
+[System.Serializable]
+public class QuestProgress
+{
+    public Quest quest;
+    public List<QuestObjective> objectives;
+
+    public QuestProgress(Quest quest)
+    {
+        this.quest = quest;
+        objectives = new List<QuestObjective>();
+
+        foreach (var ob in quest.objectives)
+        {
+            objectives.Add(new QuestObjective
+            {
+                objectiveID = ob.objectiveID,
+                description = ob.description,
+                type = ob.type,
+                requiredAmount = ob.requiredAmount,
+                currentAmount = 0
+            });
+        }
+    }
+
+    public bool isCompleted => objectives.TrueForAll(o => o.isCompleted);
+
+    public string QuestID => quest.questID;
+}
---
+++ Port_Augustine/Assets/Scripts/Quest/QuestUI.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class QuestUI : MonoBehaviour
+{
+    public Transform questListContent;
+    public GameObject questEntryPrefab;
+    public GameObject objectiveTextPrefab;
+
+    public Quest testQuest;
+    public int testQuestAmount;
+    private List<QuestProgress> testQuests = new();
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < testQuestAmount; i++)
+        {
+            testQuests.Add(new QuestProgress(testQuest));
+        }
+
+        UpdateQuestUI();
+    }
+
+    // Update is called once per frame
+    void UpdateQuestUI()
+    {
+        //destroy existing quest entries
+        foreach(Transform child in questListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        //build quest entries
+        foreach(var quest in testQuests)
+        {
+            GameObject entry = Instantiate(questEntryPrefab, questListContent);
+            TMP_Text questNameText = entry.transform.Find("QuestName").GetComponent<TMP_Text>();
+            Transform objectiveList = entry.transform.Find("ObjectiveList");
+
+            questNameText.text = quest.quest.name;
+            foreach(var objective in quest.objectives)
+            {
+                GameObject objTextGo = Instantiate(objectiveTextPrefab, objectiveList);
+                TMP_Text objText = objTextGo.GetComponent<TMP_Text>();
+
+                if(objective.requiredAmount == 0)
+                {
+                    objText.text = $"{objective.description}";
+                }
+
+                else
+                {
+                    objText.text = $"{objective.description} ({objective.currentAmount}/{objective.requiredAmount})"; //objective quest
+                }
+            }
+        }
+
+    }
+}
